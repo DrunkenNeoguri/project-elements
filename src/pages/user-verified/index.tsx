5344875fadbabbdbd2f232c3402f1599a1cdeaa4
drunken_neoguri <developneoguri@gmail.com>
2024-02-29 02:37:53 +0900

    Fix. Incorrect path and component name change - 라우터 변경에 따른 path 변경 및 컴포넌트명 변경 진행

--- src/pages/user-verified/index.tsx
+++ src/pages/user-verified/index.tsx
@@ -1,7 +1,7 @@
 import SignHeader from "../../common/components/sign-header";
 import SignUpCompleteSection from "./components/sign-up-complete-section";
 
-export default function UserSignUpComplete() {
+export default function UserVerified() {
   return (
     <>
       <SignHeader title="본인 인증 완료" />
--- src/router/user-router.tsx
+++ src/router/user-router.tsx
@@ -1,15 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import UserSignIn from "../pages/user-sign-in";
-import UserSignUp from "../pages/user-sign-up";
 import UserFind from "../pages/user-find";
 import UserChange from "../pages/user-change";
+import UserSignIn from "../pages/user-signin";
+import UserSignUp from "../pages/user-signup";
+import UserVerified from "../pages/user-verified";
 
 export default function UserRouter() {
   return (
     <Routes>
       <Route path="/signin" element={<UserSignIn />} />
       <Route path="/signup" element={<UserSignUp />} />
-      <Route path="/signup/complete" element={<UserSignUp />} />
+      <Route path="/verified" element={<UserVerified />} />
       <Route path="/find" element={<UserFind />} />
       <Route path="/change" element={<UserChange />} />
     </Routes>
