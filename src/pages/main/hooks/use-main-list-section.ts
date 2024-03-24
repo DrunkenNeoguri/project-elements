b487551c3acda46481a1848c99bca82e056b042f
drunken_neoguri <developneoguri@gmail.com>
2024-03-25 03:35:44 +0900

    [Fix] 메인 페이지에서 새로고침 할 시, 유저 정보를 받아오지 못하는 이슈 수정 #8 - firebaseAuth에서 유저 정보가 있는지 체크하도록 케이스 추가 - 없을 경우, onAuthStateChanged를 통해서 유저 정보가 있는지 체크해서 있을 경우와 없을 경우를 분리하도록 처리 ※ 자동 로그인이 아닌 경우, 새로고침을 하면 인 메모리 상태에서는 로그인 상태가 날아가므로 최소한. 자동 로그인이 아닌 상태에선 세션 스토리지에 계정 정보를 저장하도록 유지함.

--- src/pages/main/hooks/use-main-list-section.ts
+++ src/pages/main/hooks/use-main-list-section.ts
@@ -4,6 +4,7 @@ import { firebaseAuth, firestore } from "../../../utils/util-firebase";
 import { TravelListType } from "../../../common/types/template";
 import { useSearchParams } from "react-router-dom";
 import { convertUnknownTypeErrorToStringMessage } from "../../../utils/util-convert";
+import { onAuthStateChanged } from "firebase/auth";
 
 export default function useMainListSection() {
   const [traveLists, setTraveLists] = useState<TravelListType[]>([]);
@@ -14,31 +15,46 @@ export default function useMainListSection() {
     const getUserTravelLists = async () => {
       try {
         const listsArray: TravelListType[] = [];
-        const userUid = firebaseAuth.currentUser?.uid;
-        const docsState = await getDocs(
-          collection(firestore, `travels`, userUid!, "docs")
-        );
+        const user = firebaseAuth.currentUser;
 
-        docsState.forEach((doc) => {
-          const {
-            travelType,
-            title,
-            departureAt,
-            travelPeriod,
-            destination,
-            id,
-          } = doc.data();
-          listsArray.push({
-            travelType,
-            title,
-            departureAt,
-            travelPeriod,
-            destination,
-            id,
+        const getTravelListsByUsers = async (userUid: string) => {
+          const docsState = await getDocs(
+            collection(firestore, `travels`, userUid, "docs")
+          );
+
+          docsState.forEach((doc) => {
+            const {
+              travelType,
+              title,
+              departureAt,
+              travelPeriod,
+              destination,
+              id,
+            } = doc.data();
+            listsArray.push({
+              travelType,
+              title,
+              departureAt,
+              travelPeriod,
+              destination,
+              id,
+            });
           });
-        });
 
-        setTraveLists([...listsArray]);
+          return setTraveLists([...listsArray]);
+        };
+
+        if (user !== null) {
+          getTravelListsByUsers(user.uid);
+        } else {
+          onAuthStateChanged(firebaseAuth, (user) => {
+            if (user) {
+              getTravelListsByUsers(user.uid);
+            } else {
+              throw new Error("계정 정보 없음!");
+            }
+          });
+        }
       } catch (error) {
         const errorMessage = convertUnknownTypeErrorToStringMessage(error);
       }
