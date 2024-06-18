0299d1c495f70a40f8739e580026aeb6c4c85471
drunken_neoguri <developneoguri@gmail.com>
2024-06-19 00:58:18 +0900

    [Fix] 로그인 페이지 레이아웃 기능 미작동으로 인해 제거 #9

--- src/app/user/login/page.tsx
+++ src/app/user/login/page.tsx
@@ -2,11 +2,11 @@ import LoginForm from "./_components/login-form";
 import LoginHelpBox from "./_components/login-help-box";
 import LoginLangBox from "./_components/login-lang-box";
 
-export default function Page() {
+export default function Login() {
   return (
-    <>
+    <section className="flex flex-col w-full justify-center items-center">
       {/* <SignHeader title="로그인" /> */}
-      <div className="flex flex-col gap-[10px] box-border mt-6 mb-2 px-4">
+      <div className="flex flex-col gap-[10px] box-border mt-6 mb-2 w-full px-4">
         <h2 className="font-bold24 text-black">안녕하세요!</h2>
         <span className="font-medium12 text-black">
           회원이신가요? 아래의 내용을 기입하고 로그인해주세요.
@@ -15,6 +15,6 @@ export default function Page() {
       <LoginForm />
       <LoginHelpBox />
       <LoginLangBox />
-    </>
+    </section>
   );
 }
