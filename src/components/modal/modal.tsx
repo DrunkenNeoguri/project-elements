4ce2e6e39fa8b8ab0b0dbe4aa19afcfcd7b9a35d
drunken_neoguri <developneoguri@gmail.com>
2024-06-19 17:24:27 +0900

    [Fix] Modal, Loader, Portal 컴포넌트를 Modal 합성 컴포넌트로 통합 #9

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,41 +1,109 @@
-import { StModal } from "../styles/modal";
-import AlertIcon from "../../assets/icons/svg-modal-alert-icon.svg?react";
-import InfoIcon from "../../assets/icons/svg-modal-info-icon.svg?react";
-import CheckedIcon from "../../assets/icons/svg-modal-checked-icon.svg?react";
-import ConfirmIcon from "../../assets/icons/svg-modal-confirm-icon.svg?react";
-import { ModalPropType } from "../types/modal";
-import { convertSVGIconByIconType } from "../utils/modal";
-import { hasSecondaryObjectInModal } from "../policies/modal";
+import { ButtonHTMLAttributes, ReactNode, createContext } from "react";
+import Portal from "./portal";
+import {
+  ModalAlert,
+  ModalChecked,
+  ModalConfirm,
+  ModalInfo,
+} from "../../assets/icons/icons";
+import { Bar } from "../loader/loader";
 
-export default function Modal(props: ModalPropType) {
-  const { title, context, modalType, primary, secondary } = props;
-  const iconList = [
-    <InfoIcon />,
-    <ConfirmIcon />,
-    <CheckedIcon />,
-    <AlertIcon />,
-  ];
+// Modal Context API
+type ModalContextType = {
+  isOpen: boolean;
+  setIsOpen: () => void;
+};
+
+const ModalContext = createContext<ModalContextType | undefined>(undefined);
+
+// Modal
+type ModalProp = {
+  children: ReactNode;
+  isOpen: boolean;
+  setIsOpen: () => void;
+};
+
+// !TODO - tailwind filter 추가 :  drop-shadow(4px 4px 8px ${colors.shadowLight})
+function Modal(props: ModalProp) {
+  const { children, isOpen, setIsOpen } = props;
+
   return (
-    <StModal.Wrapper>
-      <StModal.Container>
-        <StModal.Title $modalType={modalType}>{title}</StModal.Title>
-        <StModal.Context>{context}</StModal.Context>
-        <StModal.IconBox>
-          {iconList[convertSVGIconByIconType(modalType)]}
-        </StModal.IconBox>
-        <StModal.PrimaryButton
-          type="button"
-          onClick={primary.func}
-          $isOneButton={hasSecondaryObjectInModal(secondary)}
-        >
-          {primary.text}
-        </StModal.PrimaryButton>
-        {hasSecondaryObjectInModal(secondary) && (
-          <StModal.SecondaryButton type="button" onClick={secondary?.func}>
-            {secondary?.text}
-          </StModal.SecondaryButton>
-        )}
-      </StModal.Container>
-    </StModal.Wrapper>
+    <ModalContext.Provider value={{ isOpen, setIsOpen }}>
+      {isOpen ?? (
+        <Portal container={document.body}>
+          <div className="bg-white flex flex-col justify-center items-center text-center border-none rounded-lg h-auto w-[calc(100%-98px)] pt-8 pb-3 px-7">
+            {children}
+          </div>
+        </Portal>
+      )}
+    </ModalContext.Provider>
   );
 }
+
+// content
+type ContentType = {
+  title: string;
+  desc: string;
+};
+
+// !TODO - tailwind 유동적 색상 변경 확인 필요
+function Content(props: ContentType) {
+  const { title, desc } = props;
+  //color: ${(props) => convertTitleColorByModalIconType(props.$modalType)};
+  return (
+    <>
+      <h2 className="font-bold18 p-0 m-0 mb-1">{title}</h2>
+      <p className="font-medium12 text-black p-0 px-[10px] m-0">{desc}</p>
+    </>
+  );
+}
+
+// dimd background
+// !TODO - keyframe animation 추가 - ${fadeIn} 0.2s;
+function Dimd() {
+  return (
+    <div className="w-full h-[100vh] bg-shadowModal flex justify-center items-center absolute z-10" />
+  );
+}
+
+// icon
+function Icon(iconType: "alert" | "info" | "confirm" | "checked") {
+  const iconList = {
+    alert: <ModalAlert />,
+    info: <ModalInfo />,
+    confirm: <ModalConfirm />,
+    checked: <ModalChecked />,
+  };
+  return <div className="w-12 h-12 m-0 mt-9 mb-7">{iconList[iconType]}</div>;
+}
+
+// modal button
+type ButtonType = ButtonHTMLAttributes<HTMLButtonElement> & {
+  text: string;
+};
+
+function ConfirmButton(props: ButtonType) {
+  const { text } = props;
+  return (
+    <button
+      className="bg-primary flex justify-center items-center w-full h-11 border-none rounded border-box font-bold16 text-white cursor-pointer"
+      {...props}
+    >
+      {text}
+    </button>
+  );
+}
+
+function SubButton(props: ButtonType) {
+  const { text } = props;
+  return <button {...props}>{text}</button>;
+}
+
+Modal.Content = Content;
+Modal.Dimd = Dimd;
+Modal.ConfirmButton = ConfirmButton;
+Modal.SubButton = SubButton;
+Modal.Icon = Icon;
+Modal.Loader = Bar;
+
+export default Modal;
--- src/components/modal/portal.tsx
+++ src/components/modal/portal.tsx
@@ -1,7 +1,13 @@
+import { ReactNode } from "react";
 import { createPortal } from "react-dom";
-import { PortalType } from "../types/portal";
 
-export default function Portal(props: PortalType) {
+export type PortalPropType = {
+  children: ReactNode;
+  container: Element | DocumentFragment;
+  key?: null | string;
+};
+
+export default function Portal(props: PortalPropType) {
   const { children, container } = props;
 
   return <>{createPortal(children, container)}</>;
