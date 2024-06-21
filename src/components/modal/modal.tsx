c1a042d45fa1aeb55c8663877691c07f0bc0486e
drunken_neoguri <developneoguri@gmail.com>
2024-06-21 18:02:32 +0900

    [Fix & Remove] 리팩토링 후 미사용 파일 제거 및 모달 내 일부 컴포넌트 스타일 수정 #9

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -23,7 +23,6 @@ type ModalProp = {
   setIsOpen: () => void;
 };
 
-// !TODO - tailwind filter 추가 :  drop-shadow(4px 4px 8px ${colors.shadowLight})
 function Modal(props: ModalProp) {
   const { children, isOpen, setIsOpen } = props;
 
@@ -31,8 +30,8 @@ function Modal(props: ModalProp) {
     <ModalContext.Provider value={{ isOpen, setIsOpen }}>
       {isOpen && (
         <Portal container={document.body}>
-          <div className="w-full h-[100vh] bg-shadowModal flex justify-center items-center absolute z-10 top-0 font-gmarketSans">
-            <div className="bg-white flex flex-col justify-center items-center text-center border-none rounded-lg h-auto w-[calc(100%-98px)] max-w-[262px] pt-8 pb-3 px-7 absolute z-20 font-gmarketSans">
+          <div className="w-full h-[100vh] bg-shadowModal flex justify-center items-center absolute z-10 top-0 font-gmarketSans animate-fadeIn">
+            <div className="bg-white flex flex-col justify-center items-center text-center border-none rounded-lg h-auto w-[calc(100%-98px)] max-w-[262px] pt-8 pb-3 px-7 absolute z-20 font-gmarketSans drop-shadow-[4px_4px_8px_#0000004D]">
               {children}
             </div>
           </div>
@@ -44,23 +43,31 @@ function Modal(props: ModalProp) {
 
 // content
 type ContentType = {
+  colorTheme: "alert" | "info" | "confirm" | "checked";
   title: string;
   desc: string;
 };
 
-// !TODO - tailwind 유동적 색상 변경 확인 필요
 function Content(props: ContentType) {
-  const { title, desc } = props;
-  //color: ${(props) => convertTitleColorByModalIconType(props.$modalType)};
+  const { title, desc, colorTheme } = props;
+
+  const titleTheme = {
+    alert: "text-error",
+    info: "text-primary",
+    confirm: "text-primary",
+    checked: "text-secondary",
+  };
+
+  const titleStyle = "font-bold18 p-0 m-0 mb-1 " + titleTheme[colorTheme];
+
   return (
     <>
-      <h2 className="font-bold18 p-0 m-0 mb-1">{title}</h2>
+      <h2 className={titleStyle}>{title}</h2>
       <p className="font-medium12 text-black p-0 px-[10px] m-0">{desc}</p>
     </>
   );
 }
 
-// !TODO - keyframe animation 추가 - ${fadeIn} 0.2s;
 // icon
 function Icon({
   iconType,
