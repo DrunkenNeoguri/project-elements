f67e0a888a23f586cf74e33671f8db62c54d0f7c
drunken_neoguri <developneoguri@gmail.com>
2024-06-24 19:29:26 +0900

    [Fix] 메인 페이지 검색 결과를 하위 페이지로 분리 #9

---
+++ src/app/main/search/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+import Header from "../../../components/header/header";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (    >
+    <>
+      <Header activeSearch={true} useSideBar={false} />
+      <section className="flex flex-col w-full justify-center items-center mt-[72px]">
+        {children}
+      </section>
+      </>
+  );
+}
