db7e49d15067ae30e0eb7da5871ac0364bdf51c2
drunken_neoguri <developneoguri@gmail.com>
2024-07-18 17:42:16 +0900

    [Fix] AuthProvider 배치를 필요한 페이지에만 쓰이도록 분산 #17

--- src/app/main/layout.tsx
+++ src/app/main/layout.tsx
@@ -1,15 +1,16 @@
 import { ReactNode } from "react";
 import Header from "../../components/header/header";
+import AuthProvider from "../../providers/auth-provider";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <AuthProvider>
       <Header activeSearch={true} useSideBar={true} />
 
       <section className="flex flex-col w-full justify-center items-center">
         {children}
       </section>
-    </>
+    </AuthProvider>
   );
 }
 ``;
--- src/app/search/layout.tsx
+++ src/app/search/layout.tsx
@@ -1,13 +1,14 @@
 import { ReactNode } from "react";
 import Header from "../../components/header/header";
+import AuthProvider from "../../providers/auth-provider";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
-    <>
+    <AuthProvider>
       <Header activeSearch={true} useSideBar={true} />
       <section className="flex flex-col w-full justify-center items-center mt-[72px]">
         {children}
       </section>
-    </>
+    </AuthProvider>
   );
 }
