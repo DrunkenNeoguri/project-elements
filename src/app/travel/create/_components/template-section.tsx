bc511a66a7386b4fd76da94366cb7f4f306caa27
drunken_neoguri <developneoguri@gmail.com>
2024-07-18 17:49:17 +0900

    [Fix] 여행 등록 쪽 External Provider 미적용된 사항 수정 #17

--- src/app/travel/create/_components/template-section.tsx
+++ src/app/travel/create/_components/template-section.tsx
@@ -15,11 +15,19 @@ export default function TemplateSection({
   handlePrevStep,
   travelData,
 }: PropType) {
-  const { isOpen, setIsOpen, user, router } = useTemplateSection();
+  const {
+    modalMsg,
+    setModalMsg,
+    externalList,
+    handleExternalList,
+    user,
+    router,
+  } = useTemplateSection();
 
   const handleMoveToMakeList = async (useTemplate: boolean) => {
     if (!user?.uid) {
-      return new Error(
+      handleExternalList("travelCreate");
+      return setModalMsg(
         `유저 정보를 확인할 수 없습니다.\n재로그인 후, 다시 여행 정보를 생성해주세요.`
       );
     }
@@ -33,7 +41,8 @@ export default function TemplateSection({
       (travelData.travelType !== "domestic" &&
         travelData.travelType !== "foreign")
     ) {
-      return new Error(
+      handleExternalList("travelCreate");
+      return setModalMsg(
         `여행을 등록하기 위한 일부 정보가 누락된 것 같아요.\n되돌아가 입력한 정보들을 재확인해주세요.`
       );
     }
@@ -47,6 +56,11 @@ export default function TemplateSection({
     if (createListState === "OK") {
       return router.push(`/elements/create?id=${travelData.id}`);
     }
+  };
+
+  const handleCloseModal = () => {
+    handleExternalList("travelCreate");
+    setModalMsg(undefined);
   };
 
   return (
@@ -82,10 +96,17 @@ export default function TemplateSection({
         </Button>
       </div>
 
-      <Modal isOpen={isOpen} setIsOpen={() => setIsOpen(false)}>
-        <Modal.Content colorTheme="alert" title="여행 정보 등록 불가" desc="" />
+      <Modal
+        isOpen={externalList.includes("travelCreate")}
+        setIsOpen={handleCloseModal}
+      >
+        <Modal.Content
+          colorTheme="alert"
+          title="여행 정보 등록 불가"
+          desc={modalMsg ?? ""}
+        />
         <Modal.Icon iconType="alert" />
-        <Modal.Button colorTheme="primary" onClick={() => setIsOpen(!isOpen)}>
+        <Modal.Button colorTheme="primary" onClick={handleCloseModal}>
           알겠습니다.
         </Modal.Button>
       </Modal>
--- src/app/travel/create/_hooks/use-template-section.tsx
+++ src/app/travel/create/_hooks/use-template-section.tsx
@@ -1,11 +1,20 @@
 import { useRouter } from "next/navigation";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../../../providers/auth-provider";
+import { ExternalContext } from "../../../../providers/external-provider";
 
 export default function useTemplateSection() {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [modalMsg, setModalMsg] = useState<string | undefined>();
+  const { externalList, handleExternalList } = useContext(ExternalContext);
   const user = useContext(AuthContext);
   const router = useRouter();
 
-  return { isOpen, setIsOpen, user, router };
+  return {
+    modalMsg,
+    setModalMsg,
+    externalList,
+    handleExternalList,
+    user,
+    router,
+  };
 }
