c818459a959a2cd3ad833432ae2c8499ca5837c3
drunken_neoguri <developneoguri@gmail.com>
2024-07-19 23:44:36 +0900

    [Fix] 준비물 데이터 로직 변경에 따른 훅 내용 수정 #17

--- src/app/element/create/_hooks/use-travel-create.ts
+++ src/app/element/create/_hooks/use-travel-create.ts
@@ -2,39 +2,35 @@ import { useContext, useEffect, useState } from "react";
 import ElementService from "../../../../services/element-service";
 import { useSearchParams } from "next/navigation";
 import { AuthContext } from "../../../../providers/auth-provider";
-import { DocumentData } from "firebase/firestore";
+import { ElementsBasicType } from "../../../../types/element.types";
 
 export default function useTravelCreate() {
-  const [travelData, setTravelData] = useState<DocumentData | undefined>({});
-  const [elementList, setElementList] = useState<DocumentData | undefined>({});
+  const [elements, setElements] = useState<ElementsBasicType>();
   const user = useContext(AuthContext);
   const searchParams = useSearchParams();
   const listId = searchParams?.get("id");
 
   useEffect(() => {
     if (listId && user?.uid) {
-      const getTravelAndElementList = async () => {
+      const getElementsData = async () => {
         try {
-          // ?CONCERN: 나눠서 받도록 하는 처리가 좀 그렇다. lists 저장할 때, travel 데이터도 저장하는 게 좋을 것 같다.
-          const dataState = await ElementService.getTravelElementListData(
+          const dataState = await ElementService.getElementsData(
             user.uid,
             listId
           );
 
-          if (dataState instanceof Error) {
-            // *MEMO: 추후 모달 설치하고 모달 메시지로 변경할 것
-            return new Error("데이터 이슈");
+          if (dataState instanceof Error || !dataState) {
+            return new Error("잘못된 데이터입니다.");
           }
 
-          setTravelData(dataState.travelData);
-          setElementList(dataState.elementListData);
+          setElements(dataState);
         } catch (error) {
           // *MEMO: 에러 메시지 출력 내용 만들 것
         }
       };
-      getTravelAndElementList();
+      getElementsData();
     }
   }, [listId, user?.uid]);
 
-  return { travelData, elementList };
+  return { elements };
 }
