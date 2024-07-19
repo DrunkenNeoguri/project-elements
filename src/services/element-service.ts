6d4097b8b86485358650960ed5967b662895ba93
drunken_neoguri <developneoguri@gmail.com>
2024-07-19 23:40:51 +0900

    [Fix] 준비물 데이터 받아오는 로직 수정, 컬렉션명 수정 #17

--- src/services/element-service.ts
+++ src/services/element-service.ts
@@ -1,26 +1,21 @@
 import { collection, doc, getDoc } from "firebase/firestore";
 import { firestore } from "../utils/util-firebase";
 import { convertUnknownTypeErrorToStringMessage } from "../utils/util-convert";
+import { ElementsBasicType } from "../types/element.types";
 
 export default class ElementService {
-  static async getTravelElementListData(userUid: string, id: string) {
+  static async getElementsData(userUid: string, id: string) {
     try {
-      // TODO: 추후, 두 데이터 통합해서 받아오도록 수정
-      // TODO: 도메인과 DB 컬렉션명이 일치하지 않으니 travels, elements로 수정
-      const travelDataState = await getDoc(
-        doc(collection(await firestore(), `travels`, userUid, "docs"), id)
+      const elementsState = await getDoc(
+        doc(collection(await firestore(), `elements`, userUid, "docs"), id)
       );
 
-      const elementListDataState = await getDoc(
-        doc(collection(await firestore(), `lists`, userUid, "docs"), id)
-      );
+      if (elementsState instanceof Error) {
+        // TODO: 에러 메시지 추후 추가
+        return;
+      }
 
-      const [travelData, elementListData] = [
-        travelDataState.data(),
-        elementListDataState.data(),
-      ];
-
-      return { travelData, elementListData };
+      return elementsState.data() as ElementsBasicType;
     } catch (error) {
       return new Error(convertUnknownTypeErrorToStringMessage(error));
     }
