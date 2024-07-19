ac5ed8414202f350a271393c714efe1693359171
drunken_neoguri <developneoguri@gmail.com>
2024-07-19 23:46:11 +0900

    [Fix] 준비물 생성 / 수정 페이지 내 준비물 리스트 섹션 컴포넌트 배치 #17

--- src/app/element/create/page.tsx
+++ src/app/element/create/page.tsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 import CategoryBottomSheet from "./_components/category-bottom-sheet";
 import ElementHeader from "./_components/element-header";
 import useTravelCreate from "./_hooks/use-travel-create";
+import ElementsSection from "./_components/list-section";
 
 export default function ElementCreate() {
-  const { travelData } = useTravelCreate();
+  const { elements } = useTravelCreate();
   const [bottomSheetData, setBottomSheetData] = useState<
     Record<string, string>
   >({});
@@ -13,16 +14,12 @@ export default function ElementCreate() {
   return (
     <>
       <ElementHeader
-        title={travelData?.title}
-        departureAt={travelData?.departureAt}
-        travelPeriod={travelData?.travelPeriod}
+        title={elements?.info.title}
+        departureAt={elements?.info.departureAt}
+        travelPeriod={elements?.info.travelPeriod}
       />
-      {/* <CategoryBottomSheet
-        isOpen={state}
-        setIsOpen={() => setState(!state)}
-        bottomSheetData={data}
-        setBottomSheetData={setData}
-      /> */}
+      <ElementsSection elements={elements?.elements} />
+
       <CategoryBottomSheet
         bottomSheetData={bottomSheetData}
         setBottomSheetData={setBottomSheetData}
