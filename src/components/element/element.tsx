eb836118c257a8e7f69c20c4e6173303041e322b
drunken_neoguri <developneoguri@gmail.com>
2024-07-30 19:56:24 +0900

    [Fix] 준비물 공용 컴포넌트에 케이스 분리, 케이스에 따른 출력 사항 배치 #17

--- src/components/element/element.tsx
+++ src/components/element/element.tsx
@@ -1,15 +1,17 @@
-import { ChangeEvent, useState } from "react";
+"use client";
+import { ChangeEvent, useContext, useState } from "react";
 import { SelectedIcon, ThreeDotsIcon } from "../../assets/icons/icons";
 import { ElementBasicType } from "../../types/element.types";
 import Input from "../input/input";
+import { ExternalContext } from "../../providers/external-provider";
 
 type ElementPropsType = ElementBasicType & {
-  state: "create" | "modify" | "basic";
+  state: "create" | "modify" | "upserting" | "checking";
 };
 
 export default function Element(props: ElementPropsType) {
   const {
-    state = "basic",
+    state = "checking",
     elementName,
     elementId,
     elementColorTheme,
@@ -17,53 +19,64 @@ export default function Element(props: ElementPropsType) {
   } = props;
 
   const [value, setValue] = useState(elementName);
+  const { handleExternalList } = useContext(ExternalContext);
+
+  const handleSwitchElementBottomSheet = () => {
+    handleExternalList("element-create-selectElement");
+  };
 
   const elementStyle = isChecked
     ? "bg-paletteSubColor" + elementColorTheme
     : "bg-gray";
 
-  if (state === "basic") {
+  const elementNameStyle = state === "upserting" && "ml-1";
+
+  if (state === "upserting" || state === "checking") {
     return (
-      <button
+      <div
         id={elementId}
-        type="button"
         className={
-          "flex items-center rounded-lg w-full gap-2 p-2 m-0 outline-none border-none " +
+          "flex items-center rounded w-full gap-2 p-2 m-0 outline-none border-none " +
           elementStyle
         }
       >
-        <div>
-          <SelectedIcon />
-        </div>
-        <span className="mt-[2px]">{elementName}</span>
+        {state === "checking" && (
+          <div>
+            <SelectedIcon />
+          </div>
+        )}
+        <span className={"mt-[2px] " + elementNameStyle}>{elementName}</span>
 
         <button
           title="준비물 상세 메뉴"
           type="button"
           className="flex justify-center items-center w-7 h-7 rounded bg-transparent text-black ml-auto"
+          onClick={handleSwitchElementBottomSheet}
         >
           <ThreeDotsIcon />
         </button>
-      </button>
+      </div>
     );
   }
 
-  const handleChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
-    return setValue(e.currentTarget.value);
-  };
+  if (state === "create" || state === "modify") {
+    const handleChangeValue = (e: ChangeEvent<HTMLInputElement>) => {
+      return setValue(e.currentTarget.value);
+    };
 
-  return (
-    <div className="flex bg-gray rounded-lg w-full gap-2 p-2 m-0">
-      <Input
-        id={elementId}
-        styles="h-7 p-4"
-        value={value}
-        onChange={handleChangeValue}
-      />
+    return (
+      <div className="flex bg-gray rounded-lg w-full gap-2 p-2 m-0">
+        <Input
+          id={elementId}
+          styles="h-7 p-4"
+          value={value}
+          onChange={handleChangeValue}
+        />
 
-      <button className="flex justify-center items-center w-7 h-7 rounded bg-primary text-white">
-        {state === "create" ? <ThreeDotsIcon /> : <ThreeDotsIcon />}
-      </button>
-    </div>
-  );
+        <button className="flex justify-center items-center w-7 h-7 rounded bg-primary text-white">
+          {state === "create" ? <ThreeDotsIcon /> : <ThreeDotsIcon />}
+        </button>
+      </div>
+    );
+  }
 }
