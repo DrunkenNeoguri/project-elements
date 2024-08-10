715411cb721453089bcc53042271851512c99bbe
drunken_neoguri <developneoguri@gmail.com>
2024-08-10 15:21:30 +0900

    [Add] 준비물 리스트를 전역으로 관리할 useReducer Context 추가 #17

---
+++ src/providers/element-provider.tsx
@@ -0,0 +1,129 @@
+"use client";
+import { Dispatch, ReactNode, createContext, useReducer } from "react";
+import { CategoryBasicType, ElementBasicType } from "../types/element.types";
+
+type ElementsContextType = {
+  elements: CategoryBasicType[];
+  dispatch: Dispatch<ElementReducerActionType>;
+};
+
+type ElementReducerActionType = {
+  type: string;
+  target: ElementBasicType | CategoryBasicType | CategoryBasicType[];
+};
+
+export const ElementContext = createContext<ElementsContextType>({
+  elements: [],
+  dispatch: () => {},
+});
+
+const elementReducer = (
+  elements: CategoryBasicType[],
+  action: ElementReducerActionType
+) => {
+  switch (action.type) {
+    case "createElement":
+      return elements.map((category) => {
+        const currentElement = action.target as ElementBasicType;
+        const categoryId = (action.target as ElementBasicType).elementId.split(
+          "-"
+        )[0];
+        if (category.categoryId === categoryId) {
+          if (
+            category.categoryElements.findIndex(
+              (element) => element.elementId === categoryId
+            ) === -1
+          ) {
+            category.categoryElements.push(currentElement);
+          }
+        }
+        return category;
+      });
+
+    case "modifyElement":
+      return elements.map((category) => {
+        const currentElement = action.target as ElementBasicType;
+        const categoryId = currentElement.elementId.split("-")[0];
+        if (category.categoryId === categoryId) {
+          category.categoryElements.map((element) => {
+            if (element.elementId === currentElement.elementId) {
+              element = currentElement;
+            }
+          });
+        }
+        return category;
+      });
+
+    case "deleteElement":
+      return elements.map((category) => {
+        const currentElement = action.target as ElementBasicType;
+        const categoryId = currentElement.elementId.split("-")[0];
+        if (category.categoryId === categoryId) {
+          category.categoryElements.filter(
+            (element) => element.elementId !== currentElement.elementId
+          );
+        }
+        return category;
+      });
+
+    case "createCategory":
+      if (
+        elements.findIndex(
+          (category) =>
+            category.categoryId ===
+            (action.target as CategoryBasicType).categoryId
+        ) === -1
+      ) {
+        elements.push(action.target as CategoryBasicType);
+        return elements;
+      }
+      return elements;
+
+    case "updateCategory":
+      return elements.map((category) => {
+        const currentCategory = action.target as CategoryBasicType;
+        if (category.categoryId === currentCategory.categoryId) {
+          category = currentCategory;
+        }
+        return category;
+      });
+
+    case "deleteCategory":
+      return elements.filter(
+        (category) =>
+          category.categoryId ===
+          (action.target as CategoryBasicType).categoryId
+      );
+
+    case "setData":
+      return (elements = action.target as CategoryBasicType[]);
+
+    case "clearData":
+      return [] as CategoryBasicType[];
+
+    default:
+      return elements;
+  }
+};
+
+export default function ElementProvider({ children }: { children: ReactNode }) {
+  const initialElements = (
+    elements: CategoryBasicType[]
+  ): CategoryBasicType[] => {
+    return elements;
+  };
+
+  const [elements, dispatch] = useReducer<
+    (
+      state: CategoryBasicType[],
+      action: ElementReducerActionType
+    ) => CategoryBasicType[],
+    CategoryBasicType[]
+  >(elementReducer, [], initialElements);
+
+  return (
+    <ElementContext.Provider value={{ elements, dispatch }}>
+      {children}
+    </ElementContext.Provider>
+  );
+}
