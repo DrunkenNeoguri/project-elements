f9c5020f53eddcee64a30e2b6eadd77b26e6b787
drunken_neoguri <developneoguri@gmail.com>
2024-08-12 00:52:31 +0900

    [Fix] 카테고리 옵션 바텀 시트에 카테고리 수정, 삭제 기능 적용 #17

--- src/app/element/create/_components/bottom-sheet/category-option-bottom-sheet.tsx
+++ src/app/element/create/_components/bottom-sheet/category-option-bottom-sheet.tsx
@@ -1,6 +1,9 @@
 import { Dispatch, SetStateAction, useContext } from "react";
 import BottomSheet from "../../../../../components/bottom-sheet/bottom-sheet";
 import { ExternalContext } from "../../../../../providers/external-provider";
+import { PartContext } from "../../../../../providers/part-provider";
+import { ElementsContext } from "../../../../../providers/elements-provider";
+import { CategoryBasicType } from "../../../../../types/element.types";
 
 type SelectBottomSheetPropsType = {
   bottomSheetData: Record<string, string>;
@@ -12,28 +15,43 @@ export default function CategoryOptionBottomSheet(
 ) {
   const { bottomSheetData, setBottomSheetData } = props;
   const { externalList, handleExternalList } = useContext(ExternalContext);
+  const { dispatch } = useContext(ElementsContext);
+  const { part, handleSetPart } = useContext(PartContext);
 
-  const handleSwitchSelectBottomSheet = () => {
-    handleExternalList("element-create-selectCategory");
+  const handleSwitchOptionBottomSheet = () => {
+    handleExternalList("element-option-category");
+    handleSetPart(null);
   };
 
-  const handleSwitchCategoryBottomSheet = () => {
-    handleExternalList("element-create-category");
+  const handleOpenUpdateBottomSheet = () => {
+    handleExternalList("element-update-category");
   };
+
+  const handleDeleteCategory = () => {
+    dispatch({
+      type: "deleteCategory",
+      target: part as CategoryBasicType,
+    });
+    handleSwitchOptionBottomSheet();
+  };
+  // ?CONCERN: 여기서는 setBottomSheetData가 필요하지 않은데.. 어떻게 할까..
 
   return (
-    externalList.has("element-create-selectCategory") && (
+    externalList.has("element-option-category") && (
       <BottomSheet
         bottomSheetData={bottomSheetData}
         setBottomSheetData={setBottomSheetData}
-        onClose={handleSwitchSelectBottomSheet}
+        onClose={handleSwitchOptionBottomSheet}
       >
         <div className="flex flex-col items-start w-full px-4 my-3">
-          <BottomSheet.Select onClick={handleSwitchCategoryBottomSheet}>
+          <BottomSheet.Select onClick={handleOpenUpdateBottomSheet}>
             카테고리 수정
           </BottomSheet.Select>
           <div className="h-[1px] w-full bg-gray" />
-          <BottomSheet.Select colorTheme="warning">
+          <BottomSheet.Select
+            colorTheme="warning"
+            onClick={handleDeleteCategory}
+          >
             카테고리 삭제
           </BottomSheet.Select>
         </div>
