ea381cfdbfa48da66a1b894db2ec13b1f05b8064
drunken_neoguri <developneoguri@gmail.com>
2024-08-12 00:35:09 +0900

    [Fix] PartProvider에 준비물 수정 등 처리 위한 setState를 포함한 타입 추가 #17

--- src/providers/part-provider.tsx
+++ src/providers/part-provider.tsx
@@ -1,28 +1,42 @@
 "use client";
-import { ReactNode, createContext, useState } from "react";
+import {
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  createContext,
+  useState,
+} from "react";
 import { CategoryBasicType, ElementBasicType } from "../types/element.types";
+import { ElementStateType } from "../components/element/element";
+
+type PartType =
+  | ElementBasicType
+  | ElementBasicType
+  | (ElementBasicType & {
+      setState: Dispatch<SetStateAction<ElementStateType>>;
+    })
+  | CategoryBasicType
+  | null;
 
 type PartContextType = {
-  part: ElementBasicType | CategoryBasicType | null;
-  handlSetPart: (part: ElementBasicType | CategoryBasicType) => void;
+  part: PartType;
+  handleSetPart: (part: PartType) => void;
 };
 
 export const PartContext = createContext<PartContextType>({
   part: null,
-  handlSetPart: () => {},
+  handleSetPart: () => {},
 });
 
 export default function PartProvider({ children }: { children: ReactNode }) {
-  const [part, setPart] = useState<ElementBasicType | CategoryBasicType | null>(
-    null
-  );
+  const [part, setPart] = useState<PartType>(null);
 
-  const handlSetPart = (currentPart: ElementBasicType | CategoryBasicType) => {
+  const handleSetPart = (currentPart: PartType) => {
     setPart(currentPart);
   };
 
   return (
-    <PartContext.Provider value={{ part, handlSetPart }}>
+    <PartContext.Provider value={{ part, handleSetPart }}>
       {children}
     </PartContext.Provider>
   );
