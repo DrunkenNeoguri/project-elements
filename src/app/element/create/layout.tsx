bfaea7cedad522dd29b938b2ffe53f5e30dd5c9d
drunken_neoguri <developneoguri@gmail.com>
2024-08-12 23:52:12 +0900

    [Fix] 준비물 리스트 편집 페이지의 헤더에 카테고리 옵션 바텀 시트 key 수정 #17

--- src/app/element/create/layout.tsx
+++ src/app/element/create/layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: { children: ReactNode }) {
   const { handleExternalList } = useContext(ExternalContext);
 
   const handleSwitchCategoryBottomSheet = () => {
-    handleExternalList("element-create-selectElementList");
+    handleExternalList("element-option-elements");
   };
 
   return (
