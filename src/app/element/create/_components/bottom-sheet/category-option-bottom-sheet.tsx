bf70fe3b00c05be246a14f53f8b3de6d40ff1a0f
drunken_neoguri <developneoguri@gmail.com>
2024-08-13 00:07:50 +0900

    [Fix] 카테고리 하나 시 삭제 기능 적용되지 않도록 예외사항 처리 #17

--- src/app/element/create/_components/bottom-sheet/category-option-bottom-sheet.tsx
+++ src/app/element/create/_components/bottom-sheet/category-option-bottom-sheet.tsx
@@ -1,22 +1,17 @@
-import { Dispatch, SetStateAction, useContext } from "react";
+import { useContext, useState } from "react";
 import BottomSheet from "../../../../../components/bottom-sheet/bottom-sheet";
 import { ExternalContext } from "../../../../../providers/external-provider";
 import { PartContext } from "../../../../../providers/part-provider";
 import { ElementsContext } from "../../../../../providers/elements-provider";
 import { CategoryBasicType } from "../../../../../types/element.types";
 
-type SelectBottomSheetPropsType = {
-  bottomSheetData: Record<string, string>;
-  setBottomSheetData: Dispatch<SetStateAction<Record<string, string>>>;
-};
-
-export default function CategoryOptionBottomSheet(
-  props: SelectBottomSheetPropsType
-) {
-  const { bottomSheetData, setBottomSheetData } = props;
+export default function CategoryOptionBottomSheet() {
   const { externalList, handleExternalList } = useContext(ExternalContext);
-  const { dispatch } = useContext(ElementsContext);
+  const { state, dispatch } = useContext(ElementsContext);
   const { part, handleSetPart } = useContext(PartContext);
+  const [bottomSheetData, setBottomSheetData] = useState<
+    Record<string, string>
+  >({});
 
   const handleSwitchOptionBottomSheet = () => {
     handleExternalList("element-option-category");
@@ -28,11 +23,13 @@ export default function CategoryOptionBottomSheet(
   };
 
   const handleDeleteCategory = () => {
-    dispatch({
-      type: "deleteCategory",
-      target: part as CategoryBasicType,
-    });
-    handleSwitchOptionBottomSheet();
+    if (state.elements.length > 1) {
+      dispatch({
+        type: "deleteCategory",
+        target: part as CategoryBasicType,
+      });
+      handleSwitchOptionBottomSheet();
+    }
   };
   // ?CONCERN: 여기서는 setBottomSheetData가 필요하지 않은데.. 어떻게 할까..
 
@@ -47,13 +44,18 @@ export default function CategoryOptionBottomSheet(
           <BottomSheet.Select onClick={handleOpenUpdateBottomSheet}>
             카테고리 수정
           </BottomSheet.Select>
-          <div className="h-[1px] w-full bg-gray" />
-          <BottomSheet.Select
-            colorTheme="warning"
-            onClick={handleDeleteCategory}
-          >
-            카테고리 삭제
-          </BottomSheet.Select>
+
+          {state.elements.length > 1 && (
+            <>
+              <div className="h-[1px] w-full bg-gray" />
+              <BottomSheet.Select
+                colorTheme="warning"
+                onClick={handleDeleteCategory}
+              >
+                카테고리 삭제
+              </BottomSheet.Select>
+            </>
+          )}
         </div>
       </BottomSheet>
     )
