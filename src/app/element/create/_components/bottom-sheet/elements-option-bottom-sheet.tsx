0e6340eee7457a32f3cb70a59ce7b5a6bffb2f6b
drunken_neoguri <developneoguri@gmail.com>
2024-08-12 23:28:23 +0900

    [Fix] 준비물 리스트 옵션 바텀 시트 파일명 변경 및 수정, 삭제 기능 반영 #17

---
+++ src/app/element/create/_components/bottom-sheet/elements-option-bottom-sheet.tsx
@@ -0,0 +1,66 @@
+import { useContext, useState } from "react";
+import BottomSheet from "../../../../../components/bottom-sheet/bottom-sheet";
+import { ExternalContext } from "../../../../../providers/external-provider";
+import { ElementsContext } from "../../../../../providers/elements-provider";
+import { useRouter } from "next/navigation";
+import ElementService from "../../../../../services/element-service";
+import { AuthContext } from "../../../../../providers/auth-provider";
+
+export default function ElementsOptionBottomSheet() {
+  const { externalList, handleExternalList } = useContext(ExternalContext);
+  const { state } = useContext(ElementsContext);
+  const user = useContext(AuthContext);
+  const router = useRouter();
+  const [bottomSheetData, setBottomSheetData] = useState<
+    Record<string, string>
+  >({});
+
+  const handleSwitchSelectBottomSheet = () => {
+    handleExternalList("element-option-elements");
+  };
+
+  const handleSwitchTravelInfoBottomSheet = () => {
+    handleExternalList("element-update-travel-info");
+  };
+
+  const handleUpdateTravelData = async () => {
+    if (user) {
+      await ElementService.postElementsData(user?.uid, state.info.id, state);
+      return router.push("/element");
+    }
+  };
+
+  const handleDeleteTravelData = async () => {
+    if (user) {
+      await ElementService.deleteElementsData(user?.uid, state.info.id);
+      return router.push("/main");
+    }
+  };
+
+  return (
+    externalList.has("element-option-elements") && (
+      <BottomSheet
+        bottomSheetData={bottomSheetData}
+        setBottomSheetData={setBottomSheetData}
+        onClose={handleSwitchSelectBottomSheet}
+      >
+        <div className="flex flex-col items-start w-full px-4 my-3">
+          <BottomSheet.Select onClick={handleSwitchTravelInfoBottomSheet}>
+            여행 정보 수정
+          </BottomSheet.Select>
+          <div className="h-[1px] w-full bg-gray" />
+          <BottomSheet.Select onClick={handleUpdateTravelData}>
+            준비물 목록 저장
+          </BottomSheet.Select>
+          <div className="h-[1px] w-full bg-gray" />
+          <BottomSheet.Select
+            colorTheme="warning"
+            onClick={handleDeleteTravelData}
+          >
+            준비물 목록 삭제
+          </BottomSheet.Select>
+        </div>
+      </BottomSheet>
+    )
+  );
+}
