ef1426c38b8c82db0b264240d40595ce03535cf8
drunken_neoguri <developneoguri@gmail.com>
2024-08-12 23:57:24 +0900

    [Fix] 페이지 내 각 BottomSheet prop 사용되지 않아 제거, 로딩 backdrop 추가 #17

--- src/app/element/create/page.tsx
+++ src/app/element/create/page.tsx
@@ -1,50 +1,44 @@
 "use client";
-import { useState } from "react";
+import { useContext } from "react";
 import ElementHeader from "./_components/element-header";
 import useTravelCreate from "./_hooks/use-travel-create";
 import ElementsSection from "./_components/elements-section";
-import ListOptionBottomSheet from "./_components/bottom-sheet/list-option-bottom-sheet";
-import ElementOptionBottomSheet from "./_components/bottom-sheet/element-option-bottom-sheet";
+
 import CategoryOptionBottomSheet from "./_components/bottom-sheet/category-option-bottom-sheet";
+import EditTravelInfoBottomSheet from "./_components/bottom-sheet/edit-travel-info-bottom-sheet";
 import EditCategoryBottomSheet from "./_components/bottom-sheet/edit-category-bottom-sheet";
+import ElementOptionBottomSheet from "./_components/bottom-sheet/element-option-bottom-sheet";
+import ElementsOptionBottomSheet from "./_components/bottom-sheet/elements-option-bottom-sheet";
+
+import { ElementsContext } from "../../../providers/elements-provider";
 import PartProvider from "../../../providers/part-provider";
+import Backdrop from "../../../components/backdrop/backdrop";
+import { Bar } from "../../../components/loader/loader";
 
 export default function ElementCreate() {
-  const { travelInfo } = useTravelCreate();
-  const [bottomSheetData, setBottomSheetData] = useState<
-    Record<string, string>
-  >({});
-
-  if (!travelInfo) {
-    return <></>;
-  }
+  useTravelCreate();
+  const { state } = useContext(ElementsContext);
 
   return (
     <>
+      {state.elements.length === 0 && (
+        <Backdrop colorTheme="loader" disabled>
+          <Bar />
+        </Backdrop>
+      )}
       <ElementHeader
-        title={travelInfo.title}
-        departureAt={travelInfo.departureAt}
-        travelPeriod={travelInfo.travelPeriod}
+        title={state?.info.title}
+        departureAt={state?.info.departureAt}
+        travelPeriod={state?.info.travelPeriod}
       />
       <PartProvider>
-        <ElementsSection />
-        {/* <SelectStaffBottomSheet
-          bottomSheetData={bottomSheetData}
-          setBottomSheetData={setBottomSheetData}
-        /> */}
+        <ElementsSection elements={state.elements} />
+        {/* <SelectStaffBottomSheet /> */}
         <EditCategoryBottomSheet />
-        <ListOptionBottomSheet
-          bottomSheetData={bottomSheetData}
-          setBottomSheetData={setBottomSheetData}
-        />
-        <ElementOptionBottomSheet
-          bottomSheetData={bottomSheetData}
-          setBottomSheetData={setBottomSheetData}
-        />
-        <CategoryOptionBottomSheet
-          bottomSheetData={bottomSheetData}
-          setBottomSheetData={setBottomSheetData}
-        />
+        <EditTravelInfoBottomSheet />
+        <ElementsOptionBottomSheet />
+        <ElementOptionBottomSheet />
+        <CategoryOptionBottomSheet />
       </PartProvider>
     </>
   );
