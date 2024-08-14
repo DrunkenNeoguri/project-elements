fa0c47b37025f370607aa4bd8b4ded331e91e1a9
drunken_neoguri <developneoguri@gmail.com>
2024-08-14 12:41:26 +0900

    [Fix] 여행 준비물 편집 페이지에서 준비물 리스트 저장 시, 체크 페이지로 넘어가도록 수정 #22

--- src/app/element/(edit)/edit/_components/bottom-sheet/elements-option-bottom-sheet.tsx
+++ src/app/element/(edit)/edit/_components/bottom-sheet/elements-option-bottom-sheet.tsx
@@ -2,7 +2,7 @@ import { useContext, useState } from "react";
 import BottomSheet from "../../../../../../components/bottom-sheet/bottom-sheet";
 import { ExternalContext } from "../../../../../../providers/external-provider";
 import { ElementsContext } from "../../../../../../providers/elements-provider";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import ElementService from "../../../../../../services/element-service";
 import { AuthContext } from "../../../../../../providers/auth-provider";
 
@@ -11,6 +11,8 @@ export default function ElementsOptionBottomSheet() {
   const { state } = useContext(ElementsContext);
   const user = useContext(AuthContext);
   const router = useRouter();
+  const searchParams = useSearchParams();
+  const listId = searchParams?.get("id");
   const [bottomSheetData, setBottomSheetData] = useState<
     Record<string, string>
   >({});
@@ -26,7 +28,7 @@ export default function ElementsOptionBottomSheet() {
   const handleUpdateTravelData = async () => {
     if (user) {
       await ElementService.postElementsData(user?.uid, state.info.id, state);
-      return router.push("/element");
+      return router.push(`/element?id=${listId}`);
     }
   };
 
