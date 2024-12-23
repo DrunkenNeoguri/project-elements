a65c52b5005cfd115a94faa6d2a17002eef4306f
developneoguri <developneoguri@gmail.com>
2024-12-23 22:38:34 +0900

    [Add] embla Carousel 라이브러리 예시에 따른 카로셀 점 관련 커스텀 훅 추가 #24

---
+++ src/app/re-main/_hooks/use-carousel-dot-state.ts
@@ -0,0 +1,38 @@
+import { useCallback, useEffect, useState } from "react";
+import { EmblaCarouselType } from "embla-carousel";
+
+type UseDotStateType = {
+  selectedIndex: number;
+  scrollSnaps: number[];
+};
+
+export const useCarouselDotState = (
+  carouselAPI: EmblaCarouselType | undefined
+): UseDotStateType => {
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [scrollSnaps, setScrollSnaps] = useState<number[]>([]);
+
+  const onInit = useCallback((carouselAPI: EmblaCarouselType) => {
+    setScrollSnaps(carouselAPI.scrollSnapList());
+  }, []);
+
+  const onSelect = useCallback((carouselAPI: EmblaCarouselType) => {
+    setSelectedIndex(carouselAPI.selectedScrollSnap());
+  }, []);
+
+  useEffect(() => {
+    if (!carouselAPI) return;
+
+    onInit(carouselAPI);
+    onSelect(carouselAPI);
+    carouselAPI
+      .on("reInit", onInit)
+      .on("reInit", onSelect)
+      .on("select", onSelect);
+  }, [carouselAPI, onInit, onSelect]);
+
+  return {
+    selectedIndex,
+    scrollSnaps,
+  };
+};
