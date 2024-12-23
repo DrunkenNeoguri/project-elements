99ecb69f912b9a165327a00b6a00e9b3d3328490
developneoguri <developneoguri@gmail.com>
2024-12-23 22:39:28 +0900

    [Fix] embla Carousel 라이브러리에 맞춰 카로셀 및 점 상태 컴포넌트 구현 및 배치 #24

--- src/app/re-main/_components/carousel-banner-section.tsx
+++ src/app/re-main/_components/carousel-banner-section.tsx
@@ -1,10 +1,54 @@
 "use client";
 
+import Autoplay from "embla-carousel-autoplay";
+import useEmblaCarousel from "embla-carousel-react";
+import { useCarouselDotState } from "../_hooks/use-carousel-dot-state";
+
 export default function CarouselBannerSection() {
+  const [carouselRef, carouselApi] = useEmblaCarousel({ loop: true }, [
+    Autoplay({ delay: 5000 }),
+  ]);
+
+  const { selectedIndex, scrollSnaps } = useCarouselDotState(carouselApi);
+
+  const mapingTest = [
+    { id: 1, url: "https://via.placeholder.com/600/92c952" },
+    { id: 2, url: "https://via.placeholder.com/600/771796" },
+    { id: 3, url: "https://via.placeholder.com/600/24f355" },
+  ];
   return (
-    <div className="flex flex-col box-border w-full ">
-      <div>스와이퍼</div>
-      <div>스와이퍼 점</div>
+    <div className="flex flex-col box-border w-full my-2">
+      <div className="overflow-hidden" ref={carouselRef}>
+        <div className="flex">
+          {mapingTest.map((props) => {
+            return (
+              <button
+                key={props.id}
+                type="button"
+                className="flex-[0_0_100%] flex bg-blue-500 rounded-lg aspect-[1/0.3803] overflow-hidden justify-center items-center drop-shadow-[0_2px_2px_rgba(0,0,0,0.1)]"
+              >
+                <img src={props.url} />
+              </button>
+            );
+          })}
+        </div>
+      </div>
+      <div className="flex justify-center items-center w- full mt-2 gap-1">
+        {scrollSnaps.map((_, index) => {
+          console.log(typeof index, typeof selectedIndex);
+          return (
+            <span
+              key={index}
+              className={
+                "h-1 rounded-full" +
+                `${
+                  index === selectedIndex ? " w-4 bg-blue-500" : " w-1 bg-gray"
+                }`
+              }
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
