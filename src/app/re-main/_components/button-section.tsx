c9979f7c71d03c2ba5ba486222caa64ca49eb291
developneoguri <developneoguri@gmail.com>
2024-12-25 23:56:40 +0900

    [Add & Fix] 여행 데이터 없을 시에 첫 생성 유도하기 위한 초기 여행 생성 안내 섹션 추가 #24

--- src/app/re-main/_components/button-section.tsx
+++ src/app/re-main/_components/button-section.tsx
@@ -1,28 +1,63 @@
 "use client";
+import { useContext, useEffect, useState } from "react";
 import { RecentlyIcon, UpcomingIcon } from "../../../assets/icons/icons";
+import { AuthContext } from "../../../providers/auth-provider";
+import { UserInfoType } from "../../../types/user.types";
+import Link from "next/link";
+import FirstCreateInfoSection from "./first-create-info-section";
 
 export default function ButtonSection() {
+  const [userData, setUserData] = useState<UserInfoType | null>(null);
+  const user = useContext(AuthContext);
+
+  useEffect(() => {
+    const userInfo = localStorage.getItem("userInfo");
+
+    if (user && userInfo != null) {
+      const parseUserInfo = JSON.parse(userInfo) as UserInfoType;
+      setUserData(parseUserInfo);
+    }
+  }, [user]);
+
+  if (!userData?.recentTravel && !userData?.upcomingTravel) {
+    return <FirstCreateInfoSection />;
+  }
+
   return (
     <section className="flex gap-4 w-full my-6">
       <div className="flex flex-col w-1/2 gap-4">
-        <button className="flex flex-col rounded-lg p-3 bg-[#C4DBFF]">
+        <Link
+          className="flex flex-col rounded-lg p-3 bg-[#C4DBFF]"
+          href={`/element?id=${userData?.recentTravel?.id}`}
+        >
           <div className="flex flex-col gap-1 items-start w-full">
             <span className="font-bold14">최근 확인했던 여행</span>
-            <span className="font-medium12">여행여행여행</span>
+            <span className="font-medium12">
+              {userData?.recentTravel?.title === ""
+                ? "등록된 여행이 없습니다."
+                : userData?.recentTravel?.title}
+            </span>
             <div className="ml-auto mr-0 mt-3">
               <RecentlyIcon />
             </div>
           </div>
-        </button>
-        <button className="flex flex-col rounded-lg p-3 bg-[#898cff80]">
+        </Link>
+        <Link
+          className="flex flex-col rounded-lg p-3 bg-[#898cff80]"
+          href={`/element?id=${userData?.upcomingTravel?.id}`}
+        >
           <div className="flex flex-col gap-1 items-start w-full">
             <span className="font-bold14">다가오는 여행</span>
-            <span className="font-medium12">여행여행여행</span>
+            <span className="font-medium12">
+              {userData?.upcomingTravel?.title === ""
+                ? "등록된 여행이 없습니다."
+                : userData?.upcomingTravel?.title}
+            </span>
             <div className="ml-auto mr-0 mt-3">
               <UpcomingIcon />
             </div>
           </div>
-        </button>
+        </Link>
       </div>
       <button className="flex flex-col rounded-lg p-3 bg-[#69af5433] w-1/2">
         <div className="flex flex-col gap-1 items-start h-full">
---
+++ src/app/re-main/_components/first-create-info-section.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function FirstCreateInfoSection() {
+  return (
+    <section className="flex flex-col w-full pt-8 pb-6 px-4 box-border gap-6">
+      <img src="/images/img-welcome-start.webp" alt="" />
+      <p className="font-medium1 text-black text-center whitespace-pre-wrap">{`아직 여행을 등록한 적이 없으시네요!\n\n아래 버튼을 눌러\n여행 일정을 등록해볼까요?`}</p>
+      <Link
+        className="flex justify-center items-center w-full h-11 rounded border-box font-bold16 cursor-pointer bg-primary text-white"
+        href="/travel/create"
+      >
+        첫 여행 등록하기
+      </Link>
+    </section>
+  );
+}
