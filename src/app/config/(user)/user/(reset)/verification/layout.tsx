fe3c960d32e5b6524ce110581426b32ae9b76103
developneoguri <developneoguri@gmail.com>
2025-01-19 16:44:45 +0900

    [Fix] 중복된 Provider 적용 제거 #25

--- src/app/config/(user)/user/(reset)/verification/layout.tsx
+++ src/app/config/(user)/user/(reset)/verification/layout.tsx
@@ -1,14 +1,13 @@
 import { ReactNode } from "react";
 import Header from "../../../../../../components/header/header";
 import AuthProvider from "../../../../../../providers/auth-provider";
-import ExternalProvider from "../../../../../../providers/external-provider";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <AuthProvider>
       <Header activePrev title="본인 확인" />
       <section className="flex flex-col w-full justify-center items-center mt-24">
-        <ExternalProvider>{children}</ExternalProvider>
+        {children}
       </section>
     </AuthProvider>
   );
