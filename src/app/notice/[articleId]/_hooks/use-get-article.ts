2a53b950612995ef3d4f0815be9232ccc19c9214
developneoguri <developneoguri@gmail.com>
2025-01-20 22:43:12 +0900

    [Add] 공지사항 상세 페이지 관련 컴포넌트 와이어프레임 추가 #25

---
+++ src/app/notice/[articleId]/_hooks/use-get-article.ts
@@ -0,0 +1,6 @@
+import { useEffect } from "react";
+
+export default function useGetArticle() {
+  useEffect(() => {}, []);
+  return {};
+}
---
+++ src/app/notice/[articleId]/page.tsx
@@ -0,0 +1,11 @@
+import ArticleBody from "./_components/article-body";
+import ArticleTitle from "./_components/article-title";
+
+export default function NoticeArticle() {
+  return (
+    <section>
+      <ArticleTitle />
+      <ArticleBody />
+    </section>
+  );
+}
