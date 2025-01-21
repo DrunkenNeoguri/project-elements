c3a23a849ab6aa0af82ded0a9425769ef7d6ac77
developneoguri <developneoguri@gmail.com>
2025-01-21 22:30:23 +0900

    [Fix] gray -> grey로 오탈자 수정 #25

--- tailwind.config.js
+++ tailwind.config.js
@@ -121,7 +121,7 @@ module.exports = {
         primaryLight: "#A6CAFF",
         secondary: "#008D18",
         white: "#FFFFFF",
-        gray: "#D9D9D9",
+        grey: "#D9D9D9",
         black: "#373737",
         error: "#D80000",
         invalid: "#909090",
