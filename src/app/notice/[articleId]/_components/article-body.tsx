d05853fa274bd534ec2da7540304d7ca957ed20e
developneoguri <developneoguri@gmail.com>
2025-01-21 22:42:05 +0900

    [Fix] 공지사항 상세 내용을 SSG로 처리될 수 있도록 로직 및 코드 수정정

--- src/app/notice/[articleId]/_components/article-body.tsx
+++ src/app/notice/[articleId]/_components/article-body.tsx
@@ -1,3 +1,18 @@
-export default function ArticleBody() {
-  return <div></div>;
+import Link from "next/link";
+import { NoticeBody } from "../../../../types/option.types";
+
+interface Props extends NoticeBody {}
+
+export default function ArticleBody({ article, href }: Props) {
+  return (
+    <section className="flex flex-col gap-4">
+      <div className="flex flex-col">{article}</div>
+      <Link
+        href={href}
+        className="flex justify-center items-center w-full h-11 rounded border-box font-bold16 cursor-pointer bg-primary text-white"
+      >
+        템플릿 구경하러 가기
+      </Link>
+    </section>
+  );
 }
--- src/app/notice/[articleId]/page.tsx
+++ src/app/notice/[articleId]/page.tsx
@@ -1,11 +1,35 @@
 import ArticleBody from "./_components/article-body";
 import ArticleTitle from "./_components/article-title";
+import OptionService from "../../../services/option-services";
 
-export default function NoticeArticle() {
+export async function generateStaticParams() {
+  const articleState = await OptionService.getNoticeArticles();
+
+  return articleState.map(({ noticeTitle }) => {
+    return {
+      articleId: noticeTitle.id,
+    };
+  });
+}
+
+export default async function NoticeArticle({
+  params: { articleId },
+}: {
+  params: { articleId: string };
+}) {
+  const noticeArticle = await OptionService.getNoticeOneArticle(articleId);
+
+  if (noticeArticle == null) {
+    return;
+  }
+
+  const { noticeTitle, noticeBody } = noticeArticle;
+
   return (
-    <section>
-      <ArticleTitle />
-      <ArticleBody />
-    </section>
+    <article className="flex flex-col gap-2 mt-1 w-full">
+      <ArticleTitle {...noticeTitle} />
+      <div className="w-full h-[1px] bg-grey" />
+      <ArticleBody {...noticeBody} />
+    </article>
   );
 }
