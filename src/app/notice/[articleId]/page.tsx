3f66754c4e3dc84badbf206231b56644e931cbad
drunken_neoguri <developneoguri@gmail.com>
2025-01-25 16:39:41 +0900

    [Fix] 마크다운 데이터를 Firebase Storage에서 받아오도록 관련된 로직을 수정 #25

--- src/app/notice/[articleId]/page.tsx
+++ src/app/notice/[articleId]/page.tsx
@@ -1,13 +1,15 @@
 import ArticleBody from "./_components/article-body";
 import ArticleTitle from "./_components/article-title";
 import OptionService from "../../../services/option-services";
+import { firebaseStorage } from "../../../utils/util-firebase";
+import { getDownloadURL, ref } from "firebase/storage";
 
 export async function generateStaticParams() {
   const articleState = await OptionService.getNoticeArticles();
 
-  return articleState.map(({ noticeTitle }) => {
+  return articleState.map(({ id }) => {
     return {
-      articleId: noticeTitle.id,
+      articleId: id,
     };
   });
 }
@@ -23,13 +25,17 @@ export default async function NoticeArticle({
     return;
   }
 
-  const { noticeTitle, noticeBody } = noticeArticle;
+  const url = await getDownloadURL(
+    ref(await firebaseStorage(), `notices/${articleId}.md`)
+  );
+  const response = await (await fetch(url)).text();
+  const { href, ...rest } = noticeArticle;
 
   return (
     <article className="flex flex-col gap-2 mt-1 w-full">
-      <ArticleTitle {...noticeTitle} />
+      <ArticleTitle {...rest} />
       <div className="w-full h-[1px] bg-grey" />
-      <ArticleBody {...noticeBody} />
+      <ArticleBody href={href} article={response} />
     </article>
   );
 }
