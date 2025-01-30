eb1597859b255a9a692e7bf5242874229eeea5fd
drunken_neoguri <developneoguri@gmail.com>
2025-01-30 17:26:17 +0900

    [Fix] 기사 로드 유무에 따른 try-catch 문 추가 #25

--- src/app/notice/[articleId]/page.tsx
+++ src/app/notice/[articleId]/page.tsx
@@ -19,24 +19,28 @@ export default async function NoticeArticle({
 }: {
   params: { articleId: string };
 }) {
-  const noticeArticle = await OptionService.getNoticeOneArticle(articleId);
+  try {
+    const noticeArticle = await OptionService.getNoticeOneArticle(articleId);
 
-  if (noticeArticle == null) {
-    // TODO: 페이지 진입 등이 잘못됐을 시, SSG인 점을 고려해서 차후 별도 기사 없음 페이지 노출 필요
-    return;
+    if (noticeArticle == null) {
+      // TODO: 페이지 진입 등이 잘못됐을 시, SSG인 점을 고려해서 차후 별도 기사 없음 페이지 노출 필요
+      return <></>;
+    }
+
+    const url = await getDownloadURL(
+      ref(await firebaseStorage(), `notices/${articleId}.md`)
+    );
+    const response = await (await fetch(url)).text();
+    const { href, ...rest } = noticeArticle;
+
+    return (
+      <article className="flex flex-col gap-2 mt-1 w-full">
+        <ArticleTitle {...rest} />
+        <div className="w-full h-[1px] bg-grey" />
+        <ArticleBody href={href} article={response} />
+      </article>
+    );
+  } catch {
+    return <></>;
   }
-
-  const url = await getDownloadURL(
-    ref(await firebaseStorage(), `notices/${articleId}.md`)
-  );
-  const response = await (await fetch(url)).text();
-  const { href, ...rest } = noticeArticle;
-
-  return (
-    <article className="flex flex-col gap-2 mt-1 w-full">
-      <ArticleTitle {...rest} />
-      <div className="w-full h-[1px] bg-grey" />
-      <ArticleBody href={href} article={response} />
-    </article>
-  );
 }
