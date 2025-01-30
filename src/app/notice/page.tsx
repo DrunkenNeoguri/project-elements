0feb1cfebcd31434eaa000c73edbbf2922fb2828
drunken_neoguri <developneoguri@gmail.com>
2025-01-30 17:10:14 +0900

    [Fix] 공지사항 서버 컴포넌트에 try-catch 적용 #25

--- src/app/notice/page.tsx
+++ src/app/notice/page.tsx
@@ -2,24 +2,29 @@ import OptionService from "../../services/option-services";
 import NoticeItem from "./_components/notice-item";
 
 export default async function Notice() {
-  const noticeTitleList = await OptionService.getNoticeItemList();
+  try {
+    const noticeTitleList = await OptionService.getNoticeItemList();
 
-  return (
-    <>
-      {noticeTitleList.map(({ id, title, clipType, createdAt }) => {
-        return (
-          <>
-            <NoticeItem
-              key={id}
-              id={id}
-              title={title}
-              clipType={clipType as "이벤트" | "공지" | "매거진"}
-              createdAt={createdAt}
-            />
-            <div className="h-[1px] w-full bg-grey" />
-          </>
-        );
-      })}
-    </>
-  );
+    return (
+      <>
+        {noticeTitleList.map(({ id, title, clipType, createdAt }) => {
+          return (
+            <>
+              <NoticeItem
+                key={id}
+                id={id}
+                title={title}
+                clipType={clipType as "이벤트" | "공지" | "매거진"}
+                createdAt={createdAt}
+              />
+              <div className="h-[1px] w-full bg-grey" />
+            </>
+          );
+        })}
+      </>
+    );
+  } catch (error) {
+    // TODO: 차후 해당 내용과 관련한 예외사항 컴포넌트트 반영 필요
+    return <></>;
+  }
 }
