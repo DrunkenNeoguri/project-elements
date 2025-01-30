8931ebe021e8c3a1936b8732d5a3c176dc3dcd6d
drunken_neoguri <developneoguri@gmail.com>
2025-01-30 17:50:53 +0900

    [Fix] 검색창 관련 접근성 개선 #25

--- src/components/header/components/search-form-field.tsx
+++ src/components/header/components/search-form-field.tsx
@@ -37,17 +37,20 @@ export function SearchFormField() {
   };
 
   return (
-    <form className="w-full ml-4" onSubmit={handleSearchKeyword}>
+    <form className="w-full ml-4" onSubmit={handleSearchKeyword} role="search">
       <input
         className="bg-invalidLight w-full h-10 font-medium16 text-black border border-invalid rounded m-0 outline-none box-border py-[10px] pl-3 pr-9 relative"
         value={keyword}
         onChange={handleChangeKeyword}
+        placeholder="검색어를 입력해주세요."
+        aria-label="검색어 입력"
       />
       <button
         type="button"
         title="관련 내용 검색"
         onClick={handleSearchButton}
         className="w-8 h-8 bg-transparent mr-0 ml-auto cursor-pointer absolute top-5 right-5"
+        aria-label="관련 내용 검색"
       >
         <ActiveSearchIcon />
       </button>
