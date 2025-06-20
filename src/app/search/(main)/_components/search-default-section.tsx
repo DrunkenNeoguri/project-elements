8d80b5f63e6647f2070eb3202b8490c96488f7a4
DrunkenNeoguri <developneoguri@gmail.com>
2025-06-20 18:02:51 +0900

    [Fix] 배너 호출 안정성 고려해 useEffect에서 특정 배너가 선정되도록 적용 #25

--- src/app/search/(main)/_components/search-default-section.tsx
+++ src/app/search/(main)/_components/search-default-section.tsx
@@ -2,16 +2,22 @@
 import Link from "next/link";
 import { ClockIcon, CloseIcon } from "../../../../assets/icons/icons";
 import { useGetCarouselBanner } from "../../../main/_hooks/use-get-carousel-banner";
-import { useMemo } from "react";
+import { useEffect, useState } from "react";
 import { useGetCurrentKeywordList } from "../_hooks/use-get-current-keyword-list";
+import { Banner } from "../../../../types/option.types";
 
 export default function SearchDefaultSection() {
   const { searchKeywordList, removeSearchKeyword, removeAllSearchKeyword } =
     useGetCurrentKeywordList();
   const { banners } = useGetCarouselBanner();
+  const [selectedBanner, setSelectedBanner] = useState<Banner | null>(null);
 
-  const banner = useMemo(() => {
-    return banners[Math.floor(Math.random() * banners.length)];
+  // 클라이언트에서만 랜덤 배너 선택
+  useEffect(() => {
+    if (banners.length > 0) {
+      const randomIndex = Math.floor(Math.random() * banners.length);
+      setSelectedBanner(banners[randomIndex]);
+    }
   }, [banners]);
 
   const hasSearchKeywordInList = searchKeywordList.length !== 0;
@@ -67,16 +73,16 @@ export default function SearchDefaultSection() {
 
       <div className="w-full h-2 bg-[#F8F8F8]" />
 
-      {banner && (
+      {selectedBanner ? (
         <div className="mt-4 mb-auto mx-4">
           <Link
             className="flex-[0_0_100%] flex bg-blue-500 rounded-lg aspect-[1/0.3803] overflow-hidden justify-center items-center drop-shadow-[0_2px_2px_rgba(0,0,0,0.1)]"
-            href={banner.href}
+            href={selectedBanner.href}
           >
-            <img src={banner.imageUrl} alt="프로모션 배너" />
+            <img src={selectedBanner.imageUrl} alt="프로모션 배너" />
           </Link>
         </div>
-      )}
+      ) : null}
     </section>
   );
 }
