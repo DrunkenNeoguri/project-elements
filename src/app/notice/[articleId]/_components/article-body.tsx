870ddd21d51320135ebe8f1a49ed9115034f4027
DrunkenNeoguri <developneoguri@gmail.com>
2025-06-20 17:45:20 +0900

    [Fix] 마크다운 중복 컴포넌트 제거 #25

--- src/app/notice/[articleId]/_components/article-body.tsx
+++ src/app/notice/[articleId]/_components/article-body.tsx
@@ -23,7 +23,6 @@ export default function ArticleBody({ article, href }: Props) {
       >
         {article}
       </Markdown>
-      <Markdown remarkPlugins={[remarkGfm]}>{`${article}`}</Markdown>
       {href ? (
         <Link
           href={href}
