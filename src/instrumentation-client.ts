bddcec12a03051a6a70eb76824842c084445947a
DrunkenNeoguri <developneoguri@gmail.com>
2025-06-21 15:19:30 +0900

    [Fix] Sentry 기본 세팅 배치 #33

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,12 @@
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: '',
+  integrations: [Sentry.replayIntegration()],
+  tracesSampleRate: 1,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+  debug: false,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
