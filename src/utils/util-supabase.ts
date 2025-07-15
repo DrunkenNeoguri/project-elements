706fb3395725a35eb38943e10f8ffa3da4705ffa
DrunkenNeoguri <developneoguri@gmail.com>
2025-07-15 18:29:27 +0900

    [Add] Supabase 유틸 추가 #34

---
+++ src/utils/util-supabase.ts
@@ -0,0 +1,21 @@
+import { createClient } from '@supabase/supabase-js';
+import { sendErrorToSentry } from './util-sentry';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+export const supabase = createClient(supabaseUrl as string, supabaseAnonKey as string);
+export const supabaseAuth = await supabase.auth;
+
+export async function supabaseDatabase(tableName: string) {
+  try {
+    return await supabase.from(tableName);
+  } catch (error) {
+    sendErrorToSentry({
+      type: 'client',
+      context: 'util-supabase',
+      error: error as Error,
+    });
+    throw new Error('Supabase Load Error: Supabase를 불러올 수 없습니다.');
+  }
+}
