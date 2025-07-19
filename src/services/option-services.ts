db0f0470132e14b82fa49f42e796e0e725739ff9
DrunkenNeoguri <developneoguri@gmail.com>
2025-07-19 16:22:14 +0900

    [Fix] option service 내 코드를 supabase 기반으로 마이그레이션 #34

--- src/services/option-services.ts
+++ src/services/option-services.ts
@@ -1,21 +1,27 @@
-import { firestore } from '../utils/util-firebase';
-import { collection, doc, getDoc, getDocs } from 'firebase/firestore';
 import { convertUnknownTypeErrorToStringMessage } from '../utils/util-convert';
 import { Banner, Notice } from '../types/option.types';
 import { getTypedDocData } from '../utils/util-safed-type';
+import { supabaseDatabase } from '../utils/util-supabase';
+import { PostgrestResponse } from '@supabase/supabase-js';
 
 // *MEMO: 문제 없이 200일 시, 예외를 제외하고 return "OK";
 class OptionService {
   static async getMainCarouselBannerList() {
     try {
-      const docsState = (await getDocs(collection(await firestore(), 'banners'))).docs.flatMap(
-        doc => {
-          const bannerData = getTypedDocData<Banner>(doc);
-          return bannerData ? [bannerData] : [];
-        },
-      );
+      const docsState = await supabaseDatabase('banners');
+      const { data: bannerList, error: bannerError }: PostgrestResponse<Banner[]> = await docsState
+        .select('*')
+        .order('order', { ascending: true });
 
-      const bannerList = docsState.sort((a, b) => a.order - b.order);
+      //* MEMO: 에러는 나중에 다시 수정합시다.
+      if (bannerError) {
+        throw new Error(
+          convertUnknownTypeErrorToStringMessage(
+            bannerError,
+            'OptionService.getMainCarouselBannerList',
+          ),
+        );
+      }
 
       return bannerList;
     } catch (error) {
@@ -26,14 +32,17 @@ class OptionService {
   }
 
   private static async getNoticesBase() {
-    const docsState = (await getDocs(collection(await firestore(), 'notices'))).docs.flatMap(
-      doc => {
-        const noticeData = getTypedDocData<Notice>(doc);
-        return noticeData ? [noticeData] : [];
-      },
-    );
+    const noticesTable = await supabaseDatabase('notices');
+    const { data: noticesList, error: noticesError }: PostgrestResponse<Notice[]> =
+      await noticesTable.select('*').order('createdAt', { ascending: false });
 
-    return docsState.sort((a, b) => Number(a.id) - Number(b.id));
+    if (noticesError) {
+      throw new Error(
+        convertUnknownTypeErrorToStringMessage(noticesError, 'OptionService.getNoticesBase'),
+      );
+    }
+
+    return noticesList;
   }
 
   static async getNoticeItemList() {
@@ -49,7 +58,10 @@ class OptionService {
 
   static async getNoticeArticles() {
     try {
-      const noticeList = await this.getNoticesBase();
+      const noticeList = await OptionService.getNoticesBase();
+      if (!noticeList || noticeList.length === 0) {
+        return [];
+      }
       return noticeList ?? [];
     } catch (error) {
       throw new Error(
@@ -60,8 +72,19 @@ class OptionService {
 
   static async getNoticeOneArticle(docId: string) {
     try {
-      const docState = await getDoc(doc(await firestore(), 'notices', docId));
-      return getTypedDocData<Notice>(docState) ?? null;
+      const noticeState = await supabaseDatabase('notices');
+      const { data: noticeData, error: noticeError }: PostgrestResponse<Notice> = await noticeState
+        .select('*')
+        .eq('id', docId)
+        .single();
+
+      if (noticeError) {
+        throw new Error(
+          convertUnknownTypeErrorToStringMessage(noticeError, 'OptionService.getNoticeOneArticle'),
+        );
+      }
+
+      return getTypedDocData<Notice>(noticeData) ?? null;
     } catch (error) {
       throw new Error(
         convertUnknownTypeErrorToStringMessage(error, 'OptionService.getNoticeOneArticle'),
