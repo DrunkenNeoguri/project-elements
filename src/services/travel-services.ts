8c33fec4063481d2db4b01ae9cbe50e83c232a0a
DrunkenNeoguri <developneoguri@gmail.com>
2025-08-11 21:20:54 +0900

    [Fix] 데이터베이스 구조 수정에 따라 여행 정보 서비스 관련 로직 수정 및 리팩토링 진행 #34

--- src/services/travel-services.ts
+++ src/services/travel-services.ts
@@ -1,95 +1,59 @@
 import { TravelBasicType } from '../types/travel.types';
 import { normalizeError } from '../utils/util-convert';
 import { basicTemplate, domesticTemplate, foreignTemplate } from '../utils/util-template';
-import { UserInfoType } from '../types/user.types';
-import { getParsedJsonData, getTypedObjectData } from '../utils/util-safed-type';
+import { getTypedObjectData } from '../utils/util-safed-type';
 import { supabase, supabaseDatabase } from '../utils/util-supabase';
 import snakecaseKeys from 'snakecase-keys';
 import camelcaseKeys from 'camelcase-keys';
 import dayjs from 'dayjs';
-
-// *MEMO: PostgreSQL을 쓰면서 테이블 구조를 전반적으로 많이 변경해야겠다는 생각이 듦.
+import { PostgrestError } from '@supabase/supabase-js';
 
 class TravelService {
   static async getUserTravelList(userId: string, keyword?: string) {
     try {
-      let travelList: TravelBasicType[] = [];
-      const travelsTable = await supabaseDatabase('travels');
-      //!CHECK: 이 구조가 적합한지 마이그레이션 이후 재확인!
-      const { data: travelsData, error: travelError } = await travelsTable
+      let query = supabaseDatabase('travels')
         .select('*')
-        .eq('userId', userId)
-        .order('departureAt', { ascending: true });
+        .eq('user_id', userId)
+        .order('departure_at', { ascending: true });
+
+      if (keyword) {
+        query = query.or(`destination.ilike.%${keyword}%,title.ilike.%${keyword}%`);
+      }
+
+      const { data: travelsData, error: travelError } = await query;
 
       if (travelError) {
         throw normalizeError(travelError, 'TravelService.getUserTravelList');
       }
 
-      travelsData.forEach(doc => {
-        const data = getTypedObjectData<TravelBasicType>(camelcaseKeys(doc));
-
-        if (data) {
-          const { travelType, title, departureAt, travelPeriod, destination, id } = data;
-          travelList.push({
-            travelType,
-            title,
-            departureAt,
-            travelPeriod,
-            destination,
-            id,
-          });
-        }
-      });
-
-      if (keyword) {
-        travelList = travelList.filter(data => {
-          return data.title.includes(keyword) || data.destination.includes(keyword);
-        });
-      }
-      return travelList;
+      return (travelsData ?? []).map(data =>
+        getTypedObjectData<TravelBasicType>(camelcaseKeys(data)),
+      );
     } catch (error) {
       throw normalizeError(error, 'TravelService.getUserTravelList');
     }
   }
 
-  static async postCreateNewTravel(
-    userId: string,
-    useTemplate: boolean,
-    formData: TravelBasicType,
-  ) {
+  static async postCreateNewTravel(userId: string, formData: TravelBasicType) {
     try {
       const template = () => {
-        if (!useTemplate) {
-          return basicTemplate;
-        }
         if (formData.travelType === 'domestic') {
           return domesticTemplate;
         }
         if (formData.travelType === 'foreign') {
           return foreignTemplate;
         }
+        return basicTemplate;
       };
 
-      const userInfo = localStorage.getItem('userInfo') ?? '';
-      const parseUserInfo = getParsedJsonData<UserInfoType>(userInfo);
-
-      const { error: transactionError } = await supabase.rpc(
+      const { data, error: transactionError } = await supabase.rpc<string, PostgrestError>(
         'create_new_travel',
         snakecaseKeys({
-          userId,
-          travelId: formData.id,
+          user_id: userId,
           travelData: {
             ...formData,
-            elements: { info: { ...formData }, elements: { ...template() } },
-          },
-          userData: {
-            ...parseUserInfo,
-            recentTravel: { title: formData.title, id: formData.id },
-            upcomingTravel: {
-              title: formData.title,
-              id: formData.id,
-              departureAt: formData.departureAt, // Ensure the date is in the correct format
-            },
+            departureAt: dayjs(formData.departureAt).toISOString(),
+            elements: template(),
           },
         }),
       );
@@ -98,61 +62,7 @@ class TravelService {
         throw normalizeError(transactionError, 'TravelService.postCreateNewTravel');
       }
 
-      const usersTable = await supabaseDatabase('users');
-      const travelsTable = await supabaseDatabase('travels');
-      const elementsTable = await supabaseDatabase('elements');
-
-      const { error: travelError } = await travelsTable.insert(
-        snakecaseKeys({
-          ...formData,
-          userId,
-        }),
-      );
-
-      const { error: elementsError } = await elementsTable.insert(
-        snakecaseKeys({
-          info: { ...formData },
-          elements: { ...template() },
-        }),
-      );
-
-      const setUpcomingTravel = () => {
-        if (parseUserInfo) {
-          if (!parseUserInfo.upcomingTravel) {
-            return {
-              title: formData.title,
-              id: formData.id,
-              departureAt: formData.departureAt,
-            };
-          } else if (
-            dayjs(parseUserInfo.upcomingTravel.departureAt).isAfter(dayjs(formData.departureAt))
-          ) {
-            return {
-              title: formData.title,
-              id: formData.id,
-              departureAt: formData.departureAt,
-            };
-          }
-          return { ...parseUserInfo.upcomingTravel };
-        }
-      };
-
-      const renewalUserData = {
-        ...parseUserInfo,
-        recentTravel: { title: formData.title, id: formData.id },
-        upcomingTravel: setUpcomingTravel(),
-      };
-
-      const { error: userError } = await usersTable
-        .update(snakecaseKeys(renewalUserData))
-        .eq('user_id', userId);
-
-      if (travelError || elementsError || userError) {
-        throw normalizeError(
-          travelError || elementsError || userError,
-          'TravelService.postCreateNewTravel',
-        );
-      }
+      return data;
     } catch (error) {
       throw normalizeError(error, 'TravelService.postCreateNewTravel');
     }
@@ -163,7 +73,7 @@ class TravelService {
       const travelsTable = await supabaseDatabase('travels');
       const { data: travelsData, error: travelsError } = await travelsTable
         .select('*')
-        .eq('id', userId)
+        .eq('user_id', userId)
         .order('departure_at', { ascending: true });
 
       if (travelsError) {
@@ -173,17 +83,9 @@ class TravelService {
       const upcomingTravel = travelsData
         .map(data => getTypedObjectData<TravelBasicType>(camelcaseKeys(data)))
         .filter((data): data is TravelBasicType => !!data)
-        .map(({ travelType, title, departureAt, travelPeriod, destination, id }) => ({
-          travelType,
-          title,
-          departureAt,
-          travelPeriod,
-          destination,
-          id,
-        }))
         .find(data => dayjs(data.departureAt).diff(dayjs()) >= 0);
 
-      return upcomingTravel;
+      return upcomingTravel?.travelId;
     } catch (error) {
       throw normalizeError(error, 'TravelService.renewalUpcomingTravelInUserData');
     }
